0dbbb3d56b8fd5a3594aa4b3f9e073f4442736f0
Susara-X- <anfuso.studio@gmail.com>
2021-04-23 16:49:44 +0200

    Preparation of the enviroment to start the project

---
+++ src/Dispatcher.java
@@ -0,0 +1,20 @@
+import java.net.*;
+import java.io.*;
+import java.util.Scanner;
+
+public class Dispatcher {
+    public static void main(String[] args) throws Exception {
+        Integer port = 6789;
+        ServerSocket welcomeSocket = new ServerSocket(port);
+
+        while(true){
+            System.out.println("\n[+] Waiting for connection");
+            Socket connection = welcomeSocket.accept();
+            System.out.println("[+] Connected with: " + connection.getInetAddress() + ":" + connection.getPort());
+
+            //passing the connection to a new worker
+            Worker sonThread = new Worker(connection);
+            sonThread.start();
+        }
+    }
+}
---
+++ src/Worker.java
@@ -0,0 +1,25 @@
+import java.net.*;
+import java.io.*;
+public class Worker extends Thread{
+    private Socket connection = null;
+    private BufferedReader inFromClient;
+    private DataOutputStream outToClient;
+
+
+    public Worker(Socket newSocket){
+        connection = newSocket;
+        try{
+            inFromClient = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            outToClient = new DataOutputStream(connection.getOutputStream());
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    public void run(){
+
+            //thread stuff
+
+    }
+}
